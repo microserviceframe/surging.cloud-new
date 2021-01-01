90453ec7cb9a7989d5b1eae2ee618364d41e1e7b
liuhongliang <1029765111@qq.com>
2021-01-01 22:18:12 +0800

    测试生成文件服务

---
+++ src/Surging.Sample/Surging.IModuleServices/Surging.IModuleServices.Common/IFileService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Surging.Cloud.CPlatform.Ioc;
+using Surging.Cloud.CPlatform.Runtime.Server.Implementation.ServiceDiscovery.Attributes;
+using Surging.Cloud.KestrelHttpServer;
+
+namespace Surging.IModuleServices.Common
+{
+    [ServiceBundle("{Service}")]
+    public interface IFileService : IServiceKey
+    {
+        [Service(EnableAuthorization = false)]
+        Task<IActionResult> Preview(string fileId);
+    }
+}
---
+++ src/Surging.Sample/Surging.Modules/Surging.Modules.Common/Domain/FileService.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Surging.Cloud.KestrelHttpServer;
+using Surging.Cloud.ProxyGenerator;
+using Surging.IModuleServices.Common;
+
+namespace Surging.Modules.Common.Domain
+{
+    public class FileService : ProxyServiceBase, IFileService
+    {
+        public async Task<IActionResult> Preview(string fileId)
+        {
+            var captchaBytes = Utils.CreateCaptcha("xxx111");
+            return new ImageResult(captchaBytes,"image/png");
+        }
+    }
+}
